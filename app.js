552e9fb148dac8bf63b55306e8f2703f18d19a26
Ibrahim <ibrahim6a@yahoo.co.uk>
2020-04-29 16:35:04 +0100

    r

--- app.js
+++ app.js
@@ -12,8 +12,8 @@ app.use(express.urlencoded( {extended: true} ));
 
 // disable CORS
 app.use(function (req, res, next) {
-    // res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Origin', req.get('origin'));
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    // res.setHeader('Access-Control-Allow-Origin', req.get('origin'));
     res.setHeader("Access-Control-Allow-Credentials", "true");
     res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
     res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
